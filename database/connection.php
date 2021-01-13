c40dce9a512be6024d4938d48e1000c4db2b6b85
JhonRenso06 <rensovasquez2014@gmail.com>
2021-01-13 15:17:09 -0500

    Cambio de rutas

--- database/connection.php
+++ database/connection.php
@@ -3,17 +3,17 @@
 setlocale(LC_ALL, "es_ES");
 date_default_timezone_set("UTC");
 date_default_timezone_set("America/Lima");
-$develop = "off";
+$develop = "on";
 if ($develop === "on") {
     define("server", "localhost");
     define("user", "root");
     define("password", "");
     define("database", "ecommerce");
-    define("ruta_imagenes", "localhost/piidelo/piidelo_backoffice/images/");
+    define("ruta_imagenes", "http://localhost/piidelo/piidelo_backoffice/images/");
 } else {
-    define("server", "https://www.piidelo.com/");
+    define("server", "localhost");
     define("user", "piidelo_root");
     define("password", "vlmA}Pn#ty?C");
     define("database", "piidelo_ecommerce");
-    define("ruta_imagenes", "https://www.piidelo.com/admin/images/");
+    define("ruta_imagenes", "https://www.pidelo.com/admin/images/");
 }
